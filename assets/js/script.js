b6dfaec73274d0601224921056f2abd3bf9962dd
Rimanshu Singh <rimanshu.singh@sourcefuse.com>
2024-12-18 11:45:02 +0530

    added book adding feature in bms

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
 let form = document.getElementById("formData");
 const formData = [];
 
-
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   const title = document.getElementById('title').value.trim();
@@ -18,13 +17,56 @@ form.addEventListener('submit', (e) => {
     return;
   }
 
-  formData.push({ title, author, isbn, publicationDate, genre });
-  console.log(formData);
-  if(formData){
-    alert("Form submitted successfully")
-  }
-  
+
+    formData.push({ title, author, isbn, publicationDate, genre });
+  updateTableData(formData);
   form.reset();
 });
 
+const updateTableData = (books) => {
+  const tableBody = document.querySelector("#tableData tbody");
+  tableBody.innerHTML = '';
+  books.forEach((data, index) => {
+    const row = document.createElement('tr');
 
+    const title = document.createElement('td');
+    title.textContent = data.title;
+    row.appendChild(title);
+
+    const author = document.createElement('td');
+    author.textContent = data.author;
+    row.appendChild(author);
+
+    const isbn = document.createElement('td');
+    isbn.textContent = data.isbn;
+    row.appendChild(isbn);
+
+    const pDate = document.createElement('td');
+    pDate.textContent = data.publicationDate;
+    row.appendChild(pDate);
+
+    const genre = document.createElement('td');
+    genre.textContent = data.genre;
+    row.appendChild(genre);
+
+    const bookAge = document.createElement('td');
+    bookAge.textContent = data.bookAge;
+    row.appendChild(bookAge);
+
+
+    const action = document.createElement('td');
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.onclick = () => deleteBook(index);
+    action.appendChild(deleteButton);
+
+    const editButton = document.createElement('button');
+    editButton.textContent = 'Edit';
+    editButton.onclick = () => editBook(index);
+    action.appendChild(editButton);
+
+    row.appendChild(action);
+    tableBody.appendChild(row);
+  });
+}
